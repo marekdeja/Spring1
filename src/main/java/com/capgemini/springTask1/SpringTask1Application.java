baf2b82487ab4eb6861a6e883279496afd92500c
Deja <marek.deja@capgemini.com>
2018-07-24 08:24:39 +0200

    Start

---
+++ src/main/java/com/capgemini/springTask1/SpringTask1Application.java
@@ -0,0 +1,12 @@
+package com.capgemini.springTask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTask1Application.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/springTask1/SpringTask1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.springTask1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTask1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
